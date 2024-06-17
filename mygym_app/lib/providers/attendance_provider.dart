741230535ac9a4c99ba51e21ea7eb442b3183638
LuisZZZeledon <luis.zeledon.gomez@est.una.ac.cr>
2024-06-17 13:40:50 -0600

    Code correction + comments

--- mygym_app/lib/providers/attendance_provider.dart
+++ mygym_app/lib/providers/attendance_provider.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: empty_catches
 
 import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:mygym_app/models/course.dart';
 import 'package:mygym_app/models/user.dart';
@@ -9,10 +10,14 @@ import 'package:http/http.dart' as http;
 import 'package:mygym_app/providers/course_provider.dart';
 
 class AttendanceProvider extends ChangeNotifier {
+
   List<Map<User, String?>> attendanceList = [];
   String baseUrl = dotenv.env['BASE_URL']!;
 
   Future<void> updateUserAttendanceStatus(User user, String? attendanceStatus) async {
+
+    
+    
     bool userExists = false;
     int existingIndex = -1;
 
@@ -29,6 +34,7 @@ class AttendanceProvider extends ChangeNotifier {
     } else {
       attendanceList.add({user: attendanceStatus});
     }
+    
   }
 
   Future<bool> registerConsecutiveAbsences(User user) async {
@@ -36,12 +42,15 @@ class AttendanceProvider extends ChangeNotifier {
   }
 
   Future<String?> getUserAttendanceList(User user) async {
+
+    
     for (Map<User, String?> attendanceEntry in attendanceList) {
       User key = attendanceEntry.keys.first;
       if (key == user) {
         return attendanceEntry[key];
       }
     }
+    
     return null;
   }
 
@@ -64,6 +73,11 @@ class AttendanceProvider extends ChangeNotifier {
     return userAttendanceStatusList;
   }
 
+  /*Future<void> clearAttendanceList() async {
+    attendanceList.clear();
+  }*/
+
+
   Future<void> setConsecutiveAbsences(User user, Course course, String? jwtToken, CourseProvider courseProvider) async {
     String userCedula = user.cedula;
     String courseId = course.id;
@@ -71,7 +85,7 @@ class AttendanceProvider extends ChangeNotifier {
     final url = '$baseUrl/api/attendaces';
 
     try {
-      // Paso 1: Obtener todos los registros de asistencia
+      // Obteniene todos los registros de asistencia
       final response = await http.get(
         Uri.parse('$url?populate=*'),
         headers: {
@@ -81,12 +95,12 @@ class AttendanceProvider extends ChangeNotifier {
       );
 
       if (response.statusCode != 200) {
-        throw Exception('Error al cargar las asistencias');
+        throw Exception('Failed to load attendances');
       }
 
       final attendances = json.decode(response.body)['data'];
 
-      // Paso 2: Encontrar el registro de asistencia específico
+      // Busca el registro de asistencia específico
       final attendance = attendances.firstWhere(
         (attendance) =>
             attendance['attributes']['user']['data']['attributes']["cedula"].toString() == userCedula &&
@@ -95,12 +109,12 @@ class AttendanceProvider extends ChangeNotifier {
       );
 
       if (attendance == null) {
-        throw Exception('Registro de asistencia no encontrado para userId $userCedula y courseId $courseId');
+        throw Exception('Attendance record not found for userId $userCedula and courseId $courseId');
       }
 
       final attendanceId = attendance['id'];
 
-      // Paso 3: Obtener los detalles del registro de asistencia específico
+      // Obteniene los detalles del registro de asistencia específico
       final attendanceResponse = await http.get(
         Uri.parse('$url/$attendanceId'),
         headers: {
@@ -110,16 +124,16 @@ class AttendanceProvider extends ChangeNotifier {
       );
 
       if (attendanceResponse.statusCode != 200) {
-        throw Exception('Error al cargar los detalles de asistencia');
+        throw Exception('Failed to load attendance details');
       }
 
       final attendanceDetails = json.decode(attendanceResponse.body)['data'];
       int consecutiveAbsences = attendanceDetails['attributes']['consecutive_absences'];
 
-      // Paso 4: Incrementar el campo de ausencias consecutivas
+      // Incrementa el campo ausencias_consecutivas
       consecutiveAbsences++;
 
-      if (consecutiveAbsences >= 3) {
+      if(consecutiveAbsences >= 3){
         await courseProvider.unenrollUserFromCourse(user, course, jwtToken);
         
         final updateResponse = await http.delete(
@@ -131,13 +145,13 @@ class AttendanceProvider extends ChangeNotifier {
         );
 
         if (updateResponse.statusCode != 200) {
-          throw Exception('Error al actualizar la asistencia');
+          throw Exception('Failed to update attendance');
         }
 
         return;
       }
 
-      // Paso 5: Actualizar el registro de asistencia con las ausencias consecutivas incrementadas
+      // Actualiza el registro de asistencia con las ausencias_consecutivas incrementadas
       final updateResponse = await http.put(
         Uri.parse('$url/$attendanceId'),
         headers: {
@@ -149,8 +163,9 @@ class AttendanceProvider extends ChangeNotifier {
         }),
       );
 
+
+
     } catch (e) {
-      // ignorar
     }
   }
 
@@ -161,7 +176,7 @@ class AttendanceProvider extends ChangeNotifier {
     final url = '$baseUrl/api/attendaces';
 
     try {
-      // Paso 1: Obtener todos los registros de asistencia
+      // Obteniene todos los registros de asistencia
       final response = await http.get(
         Uri.parse('$url?populate=*'),
         headers: {
@@ -171,12 +186,12 @@ class AttendanceProvider extends ChangeNotifier {
       );
 
       if (response.statusCode != 200) {
-        throw Exception('Error al cargar las asistencias');
+        throw Exception('Failed to load attendances');
       }
 
       final attendances = json.decode(response.body)['data'];
 
-      // Paso 2: Encontrar el registro de asistencia específico
+      // Busca el registro de asistencia específico
       final attendance = attendances.firstWhere(
         (attendance) =>
             attendance['attributes']['user']['data']['attributes']["cedula"].toString() == userCedula &&
@@ -185,12 +200,12 @@ class AttendanceProvider extends ChangeNotifier {
       );
 
       if (attendance == null) {
-        throw Exception('Registro de asistencia no encontrado para userId $userCedula y courseId $courseId');
+        throw Exception('Attendance record not found for userId $userCedula and courseId $courseId');
       }
 
       final attendanceId = attendance['id'];
 
-      // Paso 3: Obtener los detalles del registro de asistencia específico
+      // Obteniene los detalles del registro de asistencia específico
       final attendanceResponse = await http.get(
         Uri.parse('$url/$attendanceId'),
         headers: {
@@ -200,10 +215,10 @@ class AttendanceProvider extends ChangeNotifier {
       );
 
       if (attendanceResponse.statusCode != 200) {
-        throw Exception('Error al cargar los detalles de asistencia');
+        throw Exception('Failed to load attendance details');
       }
 
-      // Paso 5: Actualizar el registro de asistencia con las ausencias consecutivas reiniciadas
+      // Actualiza el registro de asistencia con las ausencias_consecutivas incrementadas
       final updateResponse = await http.put(
         Uri.parse('$url/$attendanceId'),
         headers: {
@@ -216,11 +231,10 @@ class AttendanceProvider extends ChangeNotifier {
       );
 
       if (updateResponse.statusCode != 200) {
-        throw Exception('Error al actualizar la asistencia');
+        throw Exception('Failed to update attendance');
       }
 
     } catch (e) {
-      // ignorar
     }
   }
 
@@ -229,7 +243,7 @@ class AttendanceProvider extends ChangeNotifier {
     final usersUrl = '$baseUrl/api/users';
 
     try {
-      // Obtener detalles del curso
+      // Obtiene los detalles del curso
       final courseResponse = await http.get(
         Uri.parse(courseUrl),
         headers: {
@@ -239,14 +253,14 @@ class AttendanceProvider extends ChangeNotifier {
       );
 
       if (courseResponse.statusCode != 200) {
-        throw Exception('Error al cargar los detalles del curso');
+        throw Exception('Failed to load course details');
       }
 
       final courseData = json.decode(courseResponse.body)['data']['attributes'];
       final courseUsersData = courseData['users']['data'] as List;
       final List<String> courseUserIds = courseUsersData.map((user) => user["attributes"]['cedula'] as String).toList();
 
-      // Obtener todos los usuarios
+      // Obtiene todos los usuarios
       final usersResponse = await http.get(
         Uri.parse(usersUrl),
         headers: {
@@ -256,14 +270,17 @@ class AttendanceProvider extends ChangeNotifier {
       );
 
       if (usersResponse.statusCode != 200) {
-        throw Exception('Error al cargar los usuarios');
+        throw Exception('Failed to load users');
       }
 
       final allUsersData = json.decode(usersResponse.body) as List;
       List<User> allUsers = allUsersData.map((userJson) => User.fromJson(userJson)).toList();
 
-      // Filtrar usuarios que no están inscritos en el curso
+      // Filtra los usuarios ya inscritos en el curso
       List<User> usersNotInCourse = allUsers.where((user) => !courseUserIds.contains(user.cedula)).toList();
+
+      for(var user in usersNotInCourse){
+      }
 
       return usersNotInCourse;
     } catch (e) {
@@ -275,7 +292,8 @@ class AttendanceProvider extends ChangeNotifier {
     final usersUrl = '$baseUrl/api/users';
 
     try {
-      // Obtener todos los usuarios
+      
+      // Obtiene todos los usuarios
       final usersResponse = await http.get(
         Uri.parse(usersUrl),
         headers: {
@@ -285,7 +303,7 @@ class AttendanceProvider extends ChangeNotifier {
       );
 
       if (usersResponse.statusCode != 200) {
-        throw Exception('Error al cargar los usuarios');
+        throw Exception('Failed to load users');
       }
 
       final allUsersData = json.decode(usersResponse.body) as List;
@@ -296,4 +314,285 @@ class AttendanceProvider extends ChangeNotifier {
       return [];
     }
   }
+
+  Future<List<User>> getUsersEnrolledInCourse(Course course, String? jwt) async {
+    final courseUrl = '$baseUrl/api/courses/${course.id}?populate=*';
+    final usersUrl = '$baseUrl/api/users';
+
+    try {
+      // Obtiene los detalles del curso
+      final courseResponse = await http.get(
+        Uri.parse(courseUrl),
+        headers: {
+          'Authorization': 'Bearer $jwt',
+          'Content-Type': 'application/json',
+        },
+      );
+
+      if (courseResponse.statusCode != 200) {
+        throw Exception('Failed to load course details');
+      }
+
+      final courseData = json.decode(courseResponse.body)['data']['attributes'];
+      final courseUsersData = courseData['users']['data'] as List;
+      final List<String> courseUserIds = courseUsersData.map((user) => user["attributes"]['cedula'] as String).toList();
+
+      // Obtiene todos los usuarios
+      final usersResponse = await http.get(
+        Uri.parse(usersUrl),
+        headers: {
+          'Authorization': 'Bearer $jwt',
+          'Content-Type': 'application/json',
+        },
+      );
+
+      if (usersResponse.statusCode != 200) {
+        throw Exception('Failed to load users');
+      }
+
+      final allUsersData = json.decode(usersResponse.body) as List;
+      List<User> allUsers = allUsersData.map((userJson) => User.fromJson(userJson)).toList();
+
+      // Filtra los usuarios ya inscritos en el curso
+      List<User> usersNotInCourse = allUsers.where((user) => courseUserIds.contains(user.cedula)).toList();
+
+      for(var user in usersNotInCourse){
+      }
+
+      return usersNotInCourse;
+    } catch (e) {
+      return [];
+    }
+  }
+
+  
+
+
+  
+
+
+  Future<bool> enrollClient(String selectedItem, Course course, String? jwt) async {
+    // Divide el elemento seleccionado para extraer la cédula
+    List<String> parts = selectedItem.split(' - ');
+    
+    if (parts.length < 2) {
+      return false;
+    }
+
+    String cedula = parts[1];
+
+    // Obt el ID de usuario mediante cédula
+    String userId = await fetchUserIdByCedula(cedula, jwt!);
+    if (userId == null) {
+      return false;
+    }
+
+    // Inscribe al usuario en el
+
+    final url = Uri.parse('$baseUrl/api/courses/${course.id}');
+    final headers = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer $jwt',
+    };
+    final body = jsonEncode({
+      'data': {
+        'users': {
+          'connect': [{'id': userId}]
+        }
+      }
+    });
+
+    try {
+      final response = await http.put(url, headers: headers, body: body);
+      if (response.statusCode == 200) {
+        // Crea un registro de asistencia
+        await createAttendance(userId, course.id, jwt);
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
+      return false;
+    }
+  }
+
+
+
+  Future<void> createAttendance(String userId, String courseId, String jwt) async {
+
+    final url = Uri.parse('$baseUrl/api/attendaces');
+    final headers = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer $jwt',
+    };
+
+    int userIdInt = int.parse(courseId);
+    int courseIdInt = int.parse(userId);
+
+
+    final body = jsonEncode({
+      'data': {
+        'course': {
+          'connect': [{'id': userIdInt}]
+        },
+        'user': {
+          'connect': [{'id': courseIdInt}]
+        }
+      }
+    });
+
+    try {
+      final response = await http.post(url, headers: headers, body: body);
+      if (response.statusCode == 200) {
+      } else {
+      }
+    } catch (e) {
+    }
+  }
+  
+  Future<bool> unenrollClient(String selectedItem, Course course, String? jwt) async {
+    try {
+      List<String> parts = selectedItem.split(' - ');
+    
+      if (parts.length < 2) {
+      return false;
+      }
+    // Obtiene al usuario según el ID de la cédula
+    String userCedula = parts[1];
+      String userId = await fetchUserIdByCedula(userCedula, jwt!);
+
+      // Elimina el usuario de la colección de usuarios del curso
+      bool userDisconnected = await disconnectUserFromCourse(userId, course.id, jwt);
+      if (!userDisconnected) {
+        return false;
+      }
+
+      // Elimina la línea de asistencia
+      bool attendanceDeleted = await deleteAttendance(userId, course.id, jwt);
+      if (!attendanceDeleted) {
+      }
+
+      // Devuelve true si ambas operaciones se han realizado correctamente
+      return userDisconnected && attendanceDeleted;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  Future<String> fetchUserIdByCedula(String cedula, String jwt) async {
+    try {
+      final response = await http.get(
+        Uri.parse('$baseUrl/api/users'),
+        headers: <String, String>{
+          'Authorization': 'Bearer $jwt',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        final List<dynamic> jsonResponse = jsonDecode(response.body);
+
+
+        for (var user in jsonResponse) {
+          if (user['cedula'] == cedula) {
+            return user['id'].toString();
+          }
+        }
+
+        throw Exception('User with cedula $cedula not found');
+      } else {
+        throw Exception('Failed to fetch user ID');
+      }
+    } catch (e) {
+      return "";
+    }
+  }
+
+  Future<bool> disconnectUserFromCourse(String userId, String courseId, String jwt) async {
+    try {
+      // Prepare the request body
+      Map<String, dynamic> requestBody = {
+        'data': {
+          'users': {
+            'disconnect': [{'id': userId}]
+          }
+        }
+      };
+
+      final response = await http.put(
+        Uri.parse('$baseUrl/api/courses/$courseId'),
+        headers: <String, String>{
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $jwt',
+        },
+        body: jsonEncode(requestBody),
+      );
+
+      if (response.statusCode == 200) {
+        return true;
+      } else {
+        throw Exception('Failed to disconnect user from course: ${response.statusCode}');
+      }
+    } catch (e) {
+      return false;
+    }
+  }
+
+  Future<bool> deleteAttendance(String userId, String courseId, String jwt) async {
+    int userIdInt = int.parse(userId);
+    int courseIdInt = int.parse(courseId);
+
+
+    try {
+      // Obtiene los registros de asistencia
+      final response = await http.get(
+        Uri.parse('$baseUrl/api/attendaces?populate=*'),
+        headers: <String, String>{
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $jwt',
+        },
+      );
+
+      if (response.statusCode != 200) {
+        throw Exception('Failed to fetch attendance records: ${response.statusCode}');
+      }
+
+      // Parse the response body
+      final data = jsonDecode(response.body);
+      final List attendances = data['data'];
+
+      // Find the attendance record that matches the userId and courseId
+      final attendance = attendances.firstWhere(
+        (attendance) =>
+            attendance['attributes']['user']['data']['id'] == userIdInt &&
+            attendance['attributes']['course']['data']['id'] == courseIdInt,
+        orElse: () => null,
+      );
+
+      if (attendance == null) {
+        throw Exception('No matching attendance record found');
+      }
+
+      final int attendanceId = attendance['id'];
+
+      // Delete the attendance record
+      final deleteResponse = await http.delete(
+        Uri.parse('$baseUrl/api/attendaces/$attendanceId'),
+        headers: <String, String>{
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $jwt',
+        },
+      );
+
+      if (deleteResponse.statusCode == 200) {
+        return true;
+      } else {
+        throw Exception('Failed to delete attendance: ${deleteResponse.statusCode}');
+      }
+    } catch (e) {
+      return false;
+    }
+  }
+
+
+
 }
